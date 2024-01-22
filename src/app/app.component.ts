9ee2befce9b90ea0067b7dca7db6acbb7aef1a7d
gajalakshmikarthick <gkdrithi@gmail.com>
2024-01-22 16:32:13 +0530

    additem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,9 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
   additem:any="";
   item:any="";
- empdetails=[ {id:'1',name:'employee1',deptname:'iit'},
- {id:'2',name:'employee2',deptname:'mobile work'},
- {id:'3',name:'employee3',deptname:'angular'}
+ empdetails=[ {id:'1',name:'employee1',deptname:'iit',phone:'678900000'},
+ {id:'2',name:'employee2',deptname:'mobile work',age:'20',phone:'456778899'},
+ {id:'3',name:'employee3',deptname:'angular',age:'21',phone:'457899877'}
 ];
 getdata()
   {
